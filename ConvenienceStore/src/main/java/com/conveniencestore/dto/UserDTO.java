845e554043777955907ca6eb31caef510b7e4392
small <small@imagine-OwO>
2019-10-19 15:36:21 +0900

    1. DaumMap API 활용 2. 편의점 POI 생성

---
+++ ConvenienceStore/src/main/java/com/conveniencestore/dto/UserDTO.java
@@ -0,0 +1,31 @@
+package com.conveniencestore.dto;
+
+public class UserDTO {
+
+	private String userId;
+	
+	private String userPw;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPw() {
+		return userPw;
+	}
+
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDTO [userId=" + userId + ", userPw=" + userPw + "]";
+	}
+	
+	
+}
---
+++ ConvenienceStore/src/main/java/com/conveniencestore/service/UserService.java
@@ -0,0 +1,12 @@
+package com.conveniencestore.service;
+
+import java.util.List;
+
+import com.conveniencestore.dto.UserDTO;
+
+public interface UserService {
+	
+	public int login(UserDTO userDto) throws Exception;
+	
+	
+}
